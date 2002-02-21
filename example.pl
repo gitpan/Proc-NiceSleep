37cacdba725aa6d6ee33f310a37d5a835a45183e
Josh Rabinowitz <joshr-cpan@joshr.com>
2002-02-21 23:54:18 +0000

    Import of JOSHR/Proc-NiceSleep-0.50 from CPAN.
    gitpan-cpan-distribution: Proc-NiceSleep
    gitpan-cpan-version:      0.50
    gitpan-cpan-path:         JOSHR/Proc-NiceSleep-0.50.tar.gz
    gitpan-cpan-author:       JOSHR
    gitpan-cpan-maturity:     released

--- example.pl
+++ example.pl
@@ -11,28 +11,35 @@ use strict;
 use Proc::NiceSleep qw(:all);
 
 nice(5);	# lower our priority if possible
+
 minruntime(.9);	# how long to run without interruption
-sleepfactor(.4);	
+
+sleepfactor(.4);	# sleepfactor = sleeptime / non-sleeptime, ie,
 	# successive calls of maybesleep() will cause Proc::NiceSleep to
 	# try to maintain (runtime * sleepfactor) = sleep time
 
 for (my $i=0; $i < 5; $i++) {	# pretend to do ten units of work
-	print "Working for a little while...   " . 
-		scalar(localtime(time())) . "\n";
 	dosomework();	# pretend this actually did something for a second
+	showmessage("Worked for a little while...");
 	my $slept = maybesleep(); 	# check to sleep.
 	if ($slept) { 	# maybesleep() returns 0 if it didn't sleep
-		print "Slept " . sprintf("%1.2f", $slept) . 
-			" seconds.             " .  scalar(localtime(time())) . "\n"; 
+		showmessage(sprintf("Slept %1.2f seconds.", $slept));
 	}
 } 
-print "Done working!                   " .  
-	scalar(localtime(time())) . "\n"; 
+showmessage("Done working!");
+
+print "-----------------------------------------------\n";
 print "-- Informational Data About Proc::NiceSleep: --\n";
 print Proc::NiceSleep::DumpText(); # show what went on inside 
 exit(0);	# we're all finished here for now
 
-# dosomework() just sit still for a second, pretending to do work
-sub dosomework { Proc::NiceSleep::sleep(.7); }
+########## UTILITY FUNCTIONS BELOW ################
+# dosomework() just sit still for a bit, pretending to do work 
+sub dosomework { my $r = rand(7)/10; Proc::NiceSleep::sleep(.5 + $r); }
+
+sub showmessage { 
+	my $message = shift; 
+	printf("%-30s %s\n", $message, scalar(localtime(time())));
+}
 
 
--- t/04-basicsleep.t
+++ t/04-basicsleep.t
@@ -12,18 +12,32 @@ use Test;
 
 BEGIN { plan tests => 1 };	# not needed by Test::Simple, only by Test
 
-use Proc::NiceSleep;	
+use Proc::NiceSleep qw(maybesleep minruntime sleepfactor);	
 
 ok(testsleep1()); # If we made it this far, we're ok.  
 
+# on win32 with Time::HiRes, we found that sleep() sometime
+# seemed to sleep very briefly. hence the repeats of sleep() and
+# maybesleep()
 sub testsleep1 {
 	print "Sleeping about 1 seconds...\n";	# we try to do this fast
+	minruntime(0);
+	sleepfactor(.7);
+
 	my $t1 = Proc::NiceSleep::time();
-	Proc::NiceSleep::minruntime(.1);
-	Proc::NiceSleep::sleep(.2);	# we would s
-	Proc::NiceSleep::maybesleep(.5);
+	while(Proc::NiceSleep::time() - $t1 <= 1.5) {
+		Proc::NiceSleep::sleep(.5);	
+		# successive sleeps for at least 1.5 apparent seconds
+	}
+
+	maybesleep();	# in case one of these returns too fast,
+	maybesleep();	# we call it extra times...
+	maybesleep();	# they should have no effect
+	maybesleep();	# they should have no effect
+	maybesleep();	# they should have no effect :)
+
 	my $t2 = Proc::NiceSleep::time();
 	my $t = $t2 - $t1;
-	return ($t > .5);
+	return ($t > 2.0);	# maybesleep()s should have slept ~1.5 seconds
 }
 
